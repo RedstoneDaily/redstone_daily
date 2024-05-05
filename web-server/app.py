92cef4cc8c49232dc8e06a42bf3cca8176d93877
creepebucket <creepebucket@qq.com>
2024-05-06 00:49:32 +0800

    fix: 因generator.update_database_list()更改, 所以更新app.py的引用

--- web-server/app.py
+++ web-server/app.py
@@ -133,13 +133,17 @@ def query():
     with open('../backend/engine/data/database_list.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
+    res = []  # 初始化搜索结果列表
     # 遍历数据列表，查找匹配的日期
     for i in data:
         y, m, d = i.split('-')
         if int(start_date[0]) <= int(y) <= int(stop_date[0]):
             if int(start_date[1]) <= int(m) <= int(stop_date[1]):
                 if int(start_date[2]) <= int(d) <= int(stop_date[2]):
-                    return get_title(y, m, d)
+                    res.append(i)
+
+    return jsonify(res)  # 返回搜索结果列表
+
 
 
     # 未找到匹配日期，返回错误信息
@@ -159,36 +163,6 @@ def list():
         data = json.load(f)
 
     return jsonify(data)  # 返回日报列表
-
-def get_title(y, m, d):
-    """
-    获取制定日期的日报标题。
-    参数:
-    - yy: 年份，字符串格式
-    - mm: 月份，字符串格式
-    - dd: 日，字符串格式
-    返回值:
-    - 如果找到对应日期的视频标题，则返回一个包含标题的JSON响应。
-    """
-    # 将转义后的年月日拼接成日期字符串
-    date_string = y + '-' + m + '-' + d
-    # 打开并读取数据列表文件
-    with open('../backend/engine/data/database_list.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
-    # 遍历数据列表，查找匹配的日期
-    for i in data:
-        if i == date_string:
-            # 打开并读取对应日期的数据文件
-            with open('../backend/engine/data/database/' + date_string + '.json', 'r', encoding='utf-8') as f:
-                data = json.load(f)
-                return jsonify({"title":data['content'][0]['title']})  # 找到匹配日期，返回数据
-
-    # 未找到匹配日期，返回错误信息
-    response = jsonify({"error": "not found"})
-    response.status_code = 404
-    return response
-
 
 @app.route('/', methods=['GET'])
 def index():
